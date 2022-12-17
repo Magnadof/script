5d46bf7ab7e7a426c7fe38fda613459daac90909
Higor <106169820+Magnadof@users.noreply.github.com>
2022-12-17 18:45:03 -0300

    Update index.js

--- api/core/index.js
+++ api/core/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello World!!'),
+        statusCode: 200,asaas
+        body: JSON.stringify('pepapig'),
     };
     return response;
 };
